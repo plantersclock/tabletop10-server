17f2841f7fbf8b0bb5ce44e0791d533aa9bb11b9
Matthew Wright <mattman861@gmail.com>
2021-08-24 11:37:12 -0400

    added samesite and secure to cookie responses

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -49,6 +49,14 @@ router.post("/login", async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
+        sameSite:
+          process.env.NODE_ENV === "development"
+            ? "lax"
+            : process.env.NODE_ENV === "production" && "none",
+        secure:
+          process.env.NODE_ENV === "development"
+            ? false
+            : process.env.NODE_ENV === "production" && true,
       })
       .send();
   } catch (error) {
@@ -112,6 +120,14 @@ router.post("/signup", async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
+        sameSite:
+          process.env.NODE_ENV === "development"
+            ? "lax"
+            : process.env.NODE_ENV === "production" && "none",
+        secure:
+          process.env.NODE_ENV === "development"
+            ? false
+            : process.env.NODE_ENV === "production" && true,
       })
       .send();
   } catch (error) {
